a2d61801c8f54478657ae97504573d88c65fb0d3
mbignon <n60955mb@E-LOSXUL9Q6L4.local>
2022-09-14 11:14:30 +0100

    Add comments to the main executable file

--- run_kwn.sh
+++ run_kwn.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+
+# the python code just reads the inputs in the yaml file "input.yaml" and geenrates a textfile ('input.dat') with the same inputs formatted to be readable for the main program a.out
 python generate_input_fortran.py
+# this is the main program
 ./a.out
