1641d06c6601b7e057875e3a3f25ce42c1baf9b6
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2023-01-09 19:52:42 +0000

    expand basic test script

--- run_basic_test.sh
+++ run_basic_test.sh
@@ -1,16 +1,24 @@
-scenario="tests/test_1a"
+#!/bin/bash
 
-./run_kwn.sh ${scenario}
 
+run_analysis ()
+{
+./run_kwn.sh ${scenario} 2>&1 > ${scenario}_log.txt 
 echo "checking output file checksums"
-
 cd ${scenario}/results || output="no result folder" && output=$(diff ../reference_result_checksums.txt <(md5 *))
-
 if [ -z "${output}" ];then
     echo "${scenario} outputs match reference outputs"
 else
     echo ${output}
 fi
+}
+
+scenario="tests/test_1a"
+run_analysis()
+
+scenario="tests/test_2a"
+run_analysis()
+
 
 echo "tests finished"
 
