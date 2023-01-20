960983e53b2026a650c4a8bf417e8d4c20f9d56f
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2023-01-20 13:35:04 +0000

    example binary/ternary beta_star test split

--- test/test_suite1.f90
+++ test/test_suite1.f90
@@ -17,7 +17,8 @@ subroutine collect_suite1(testsuite)
 
   testsuite = [ &
     new_unittest("shear modulus", test_shear_modulus), &
-    new_unittest("beta star", test_beta_star) &
+    new_unittest("beta star binary", test_beta_star_binary), &
+    new_unittest("beta star ternary", test_beta_star_ternary) &
     ]
 
 end subroutine collect_suite1
@@ -38,7 +39,7 @@ subroutine test_shear_modulus(error)
 end subroutine test_shear_modulus
 
 
-subroutine test_beta_star(error)
+subroutine test_beta_star_binary(error)
   use KWN_model_functions, only: calculate_beta_star
   type(error_type), allocatable, intent(out) :: error
 
@@ -64,6 +65,37 @@ subroutine test_beta_star(error)
   expected = 0.310280E-18
   call check(error, val, expected, rel=.true., thr=0.001_qp)
 
-end subroutine test_beta_star
+end subroutine test_beta_star_binary
+
+
+subroutine test_beta_star_ternary(error)
+  use KWN_model_functions, only: calculate_beta_star
+  type(error_type), allocatable, intent(out) :: error
+
+  real(qp) :: val
+  real(qp) :: expected
+  
+  real(qp) :: radius_crit = 1d-9
+  real(qp) :: lattice_param = 3
+  real(qp), dimension(:), allocatable :: diffusion_coefficient
+  real(qp), dimension(:,:), allocatable :: c_matrix
+  integer :: en = 1
+
+  allocate(diffusion_coefficient(2))
+  allocate(c_matrix(2,1))
+
+  diffusion_coefficient(:) = 2
+  c_matrix(1,:) = 1
+  c_matrix(2,:) = 1
+  
+  val = calculate_beta_star(radius_crit, lattice_param, diffusion_coefficient, &
+                            c_matrix, en)
+  
+  expected = 0.155140E-18
+  call check(error, val, expected, rel=.true., thr=0.001_qp)
+
+end subroutine test_beta_star_ternary
+
+
 
 end module test_suite1
